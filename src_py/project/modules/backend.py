bce32c115792c0967927042528832c5ca5c05f37
Rzucky <75706694+Rzucky@users.noreply.github.com>
2021-07-27 16:18:28 +0200

    Mostly preventing SQL injection

--- src_py/project/modules/backend.py
+++ src_py/project/modules/backend.py
@@ -1,5 +1,5 @@
 from hat import aio
-from hat import json
+from hat import json as hatJson
 from hat.event.server import common
 from pathlib import Path
 import datetime
@@ -9,12 +9,13 @@ import os
 import sqlite3
 import time
 import typing
-
+import json
+import hat.aio
 json_schema_id = None
 json_schema_repo = None
 
 
-async def create(conf: json.Data) -> 'Backend':
+async def create(conf: hatJson.Data) -> 'Backend':
     """Creates new Backend instance with connection to database
     Args:
         conf: backend configuration
@@ -53,6 +54,8 @@ def _init_db(conf):
 class Backend(common.Backend):
 
     last_entry = {}
+    table_values = ["BUS", "LINE", "TRANSFORMER", "SWITCH"]
+    #executor = hat.aio.create_executor(1)
 
     @property
     def async_group(self) -> aio.Group:
@@ -89,42 +92,67 @@ class Backend(common.Backend):
         for e in events:            
             event_type = e.event_type
             
-            if event_type[-3] == "simulator":
-                asduAddress = int(event_type[-2])
-                ioAddress = int(event_type[-1])
-                key = f'{asduAddress},{ioAddress}'
-                data = e.payload.data
+            if event_type[-3] != "simulator":
+                continue
+        
+            asdu_address = int(event_type[-2])
+            io_address = int(event_type[-1])
+            key = f'{asdu_address},{io_address}'
+            data = json.loads(e.payload.data)
 
-                current_time = datetime.datetime.fromtimestamp(time.time())
-                containsKey = Backend.last_entry.__contains__(key)
+            current_time = datetime.datetime.fromtimestamp(time.time())
+            contains_key = Backend.last_entry.__contains__(key)
 
-                if not containsKey or current_time - datetime.timedelta(seconds=20) > Backend.last_entry[key]:
-                    Backend.last_entry[key] = current_time
+            if not contains_key or current_time - datetime.timedelta(seconds=20) > Backend.last_entry[key]:
+                Backend.last_entry[key] = current_time
 
+                table = ""
+                if 0 <= asdu_address < 10:
+                    table = "BUS"
+                elif 10 <= asdu_address < 20:
+                    table = "LINE"
+                elif asdu_address == 20:
+                    table = "TRANSFORMER"
+                elif 30 <= asdu_address < 40:
+                    table = "SWITCH"
+                # else:
+                #     ##check
+                #     table = "ERRORS"
+
+                if asdu_address in range(30, 40):
+                    data["value"] = 1 if data["value"] == "ON" else 0
+
+                if not(data["value"] == 'ON' or data["value"] == 'OFF'):
                     if math.isnan(data["value"]):
                         data["value"] = 0
-                    
-                    if asduAddress in range(0, 10): 
-                        table = "BUS"
-                    elif asduAddress in range(10, 20):
-                        table = "LINE"
-                    elif asduAddress in range(30, 40):
-                        table = "SWITCH"
-                    else:
-                        table = "TRANSFORMER"
-                    
-                    data = float(data["value"])
 
-                    entries = cur.execute(f"SELECT Count(*) FROM {table};")
-                    entries = entries.fetchone()[0]
-                    if entries > 50000:
-                        cur.execute(f"DELETE FROM {table} ORDER BY time LIMIT 1000")
+                data = float(data["value"])
+                #entries = await self.executor(fn, f"SELECT Count(*) FROM {table};")
+                #entries = await fn('SELECT COUNT(*) FROM', '{table}')
 
-                    cur.execute(f'INSERT INTO {table} (asdu, io, val) VALUES ({asduAddress}, {ioAddress}, {data});')
                 
+                #preventing all cases of sql injection
+                if table not in self.table_values:
+                    breakpoint()
+                    raise ValueError('potencial sql injection attack, given table name: ', table)
+
+                entries = cur.execute(f"SELECT Count(*) FROM {table}")
+                entries = entries.fetchone()[0]
+                if entries > 50000:
+                    cur.execute(f"DELETE FROM {table} ORDER BY time LIMIT 1000")
+
+                cur.execute(f'INSERT INTO {table} (asdu, io, val) VALUES (?, ?, ?)', (asdu_address, io_address, data))
+                
+                    # breakpoint()
+                    # # if not isinstance(data["value"], float):
+                    # #     breakpoint() 
+                    # try:
+                    #     math.isnan(data["value"])
+                    # except:
+                    #     breakpoint()
         self._db_con.commit()
-        result = events
-        return await self._async_group.spawn(aio.call, lambda: result)
+        # result = events
+        return await self._async_group.spawn(aio.call, lambda: events)
 
     async def query(self, data: common.QueryData) -> typing.List[common.Event]:
         """Gets all records for the given Query and returns them in a list
@@ -140,27 +168,34 @@ class Backend(common.Backend):
         event_type = data.event_types[0]
 
         if event_type[0] == "db":
-            asduAddress = int(event_type[1])
+            asdu_address = int(event_type[1])
+            #breakpoint()
 
-            if asduAddress in range(0, 10): 
+            if asdu_address in range(0, 10): 
                 table = "BUS"
-            elif asduAddress in range(10, 20):
+            elif asdu_address in range(10, 20):
                 table = "LINE"
-            elif asduAddress in range(30, 40):
+            elif asdu_address in range(30, 40):
                 table = "SWITCH"
             else:
                 table = "TRANSFORMER"
                 
             time_val = []
 
+            #preventing all cases of sql injection
+            if table not in self.table_values:
+                raise ValueError('potencial sql injection attack, given table name: ', table)
+
             for asdu, val, io, time in cur.execute(
-                f"SELECT asdu,io, val, time FROM {table} WHERE asdu={asduAddress} AND time >= datetime('now','-20 minutes','localtime');"):
+                f"SELECT asdu, io, val, time FROM {table} WHERE asdu = {asdu_address} AND time >= datetime('now','-20 minutes','localtime')"):
+                if asdu in range(30, 40):
+                    val = "ON" if val == 1 else "OFF"
                 time_val.append(f"{asdu};{io};{time};{val}")
 
             event = hat.event.common.Event(
                 event_id=hat.event.common.EventId(server=1, instance=1),
                 timestamp=common.Timestamp(1,2),
-                event_type=("db",),
+                event_type=("db",), 
                 source_timestamp=None,
                 payload=hat.event.common.EventPayload(
                     type=hat.event.common.EventPayloadType.JSON, data=time_val
